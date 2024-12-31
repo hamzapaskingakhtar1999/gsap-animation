fad2d8bac26bb620b3eeb53dedb6c5ff75d3c073
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-12-31 03:14:57 +0000

    image div with overlay technique to avoid gitter

--- cursor-animation/script.js
+++ cursor-animation/script.js
@@ -7,5 +7,28 @@ main.addEventListener("mousemove", (e) => {
   gsap.to("#cursor", {
     x: e.x,
     y: e.y,
+    ease: "back.out",
+  });
+});
+
+/* Now trying the cursor changes to View More once it enters an image div */
+
+const image = document.getElementById("image");
+
+image.addEventListener("mouseenter", (e) => {
+  cursor.innerHTML = "View More";
+  gsap.to("#cursor", {
+    scale: 2,
+    backgroundColor: "lightgray",
+    color: "black",
+  });
+});
+
+image.addEventListener("mouseleave", (e) => {
+  cursor.innerHTML = "";
+  gsap.to("#cursor", {
+    scale: 1,
+    backgroundColor: "tomato",
+    color: "black",
   });
 });
